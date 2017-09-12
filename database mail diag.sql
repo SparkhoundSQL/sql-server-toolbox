8c89f013739e5e73fa362e018dacbefabc4c12b4
william assaf <6641470+williamadba@users.noreply.github.com>
2017-09-12 08:38:38 -0500

    improv comments, add utility

--- database mail diag.sql
+++ database mail diag.sql
@@ -5,15 +5,17 @@ EXECUTE msdb.dbo.sysmail_help_status_sp ;
 
 GO
 
-SELECT m.recipients, m.subject, m.body, 
-    m.send_request_date, m.send_request_user, m.sent_status
+--Find recent unsent emails
+SELECT m.send_request_date, m.recipients, m.copy_recipients, m.blind_copy_recipients
+, m.[subject], m.send_request_user, m.sent_status
 FROM msdb.dbo.sysmail_allitems m
-WHERE m.sent_status<>'sent'
---WHERE m.send_request_date > CURRENT_TIMESTAMP - 3
---AND m.sent_status <> 'sent'
-ORDER BY m.send_request_date DESC
+WHERE	m.send_request_date > dateadd(day, -3, sysdatetime()) -- Only show recent day(s)
+AND		m.sent_status<>'sent' -- Possible values are sent (successful), unsent (in process), retrying (failed but retrying), failed (no longer retrying)
+ORDER BY m.send_request_date DESC;
 GO
 
 
 --Send mail test
 --exec msdb.dbo.sp_send_dbmail @profile_name ='hotmail', @recipients ='williamdassaf@hotmail.com', @subject ='test', @body = 'test'
+
+--ALTER DATABASE msdb SET ENABLE_BROKER;
