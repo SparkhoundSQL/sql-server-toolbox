cf649097a4738c29e9603b479d0fcfaf6d614e2b
william assaf <6641470+williamadba@users.noreply.github.com>
2017-10-03 11:07:54 -0500

    formatting and setup comments, rewrite with invoke-cmd

--- Availability Groups/powershell AAG failover working.ps1
+++ Availability Groups/powershell AAG failover working.ps1
@@ -1,127 +1,87 @@
-﻿#Import-Module SQLPS
+﻿<#
+Invoke-Command -script {Install-WindowsFeature -Name "Failover-Clustering" } `
+-ComputerName SQLDEV11, SQLDEV12, SQLDEV14, SQLDEV15
+Invoke-Command -script {Install-WindowsFeature -Name "RSAT-Clustering-Mgmt" } `
+-ComputerName SQLDEV11, SQLDEV12, SQLDEV14, SQLDEV15
+Invoke-Command -script {Install-WindowsFeature -Name "RSAT-Clustering-PowerShell" } `
+-ComputerName SQLDEV11, SQLDEV12, SQLDEV14, SQLDEV15
+#>
+#Install-Module SQLSERVER -Force -AllowCLobber 
+#Import-Module SQLSERVER
 
-#0-- Test AG
-    #Setup
-    $PrimaryReplicaName = "SQLSERVER-0"
-    $PrimaryReplicaInstanceName = "DEFAULT"
-    $SecondaryReplicaName1 = "SQLSERVER-1"
-    $SecondaryReplicaInstanceName1 = "DEFAULT"
-    $AvailabilityGroupName = "WWI2017-AG"
+#Must run on the primary node
+#TODO: configure initial variable values.
 
-    Set-Location "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\"
+Write-Output "Begin $(Get-Date)"
+#Setup: TODO Configure these
+$PrimaryReplicaName = "SQLSERVER-0"
+$PrimaryReplicaInstanceName = "SQL2K17" #Named instance or DEFAULT for the default instance
+$SecondaryReplicaName1 = "SQLSERVER-1"
+$SecondaryReplicaInstanceName1 = "SQL2K17" #Named instance or DEFAULT for the default instance
+$AvailabilityGroupName = "WWI2017-clusterless"
 
-    #In the following line, the %5C character replaces the \ between the SQL Server name and instance name. Passing in \ is invalid, as the \ is in the string, but is confused for a folder path.
-    Get-ChildItem "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\$($PrimaryReplicaName+$('%5C')+$PrimaryReplicaInstanceName)" `
-    | Test-SqlAvailabilityReplica 
+#Inventory and test
+Get-ChildItem "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\" | Test-SqlAvailabilityReplica | Format-Table
 
-#1-- Planned failover.
-
-    
-    #Setup
-    $PrimaryReplicaName = "SQLSERVER-0"
-    $PrimaryReplicaInstanceName = "DEFAULT"
-    $SecondaryReplicaName1 = "SQLSERVER-1"
-    $SecondaryReplicaInstanceName1 = "DEFAULT"
-    $PrimaryReplica = Get-Item "AvailabilityReplicas\$($PrimaryReplicaName)"
-    $SecondaryReplica1 = Get-Item "AvailabilityReplicas\$($SecondaryReplicaName1)"
-    Set-Location "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\"
-
-    Get-ChildItem "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\"  | Test-SqlAvailabilityReplica
-
-    #1-1-- Make the intended failover target temporarily Synchronous in an attempt to prevent data loss. 
-    Set-SqlAvailabilityReplica `
-        -Path "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\$($SecondaryReplicaName1)" `
-        -AvailabilityMode "SynchronousCommit" `
-        -FailoverMode "Manual" `
-        -ErrorAction Stop
-    
-    Set-SqlAvailabilityReplica `
-        -Path "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\$($PrimaryReplicaName)" `
-        -AvailabilityMode "SynchronousCommit" `
-        -FailoverMode "Manual" `
-        -ErrorAction Stop
-
-    write-host "Waiting"
-    Start-Sleep -s 10
+   $AGPrimaryObjPath = "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\$($PrimaryReplicaName+$(IF($PrimaryReplicaInstanceName -ne "DEFAULT"){$("%5C")+$PrimaryReplicaInstanceName} ))"
+   $AGPrimaryObj = Get-Item $AGPrimaryObjPath 
+   $AGSecondaryObjPath = "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\$($SecondaryReplicaName1+$(IF($SecondaryReplicaInstanceName1 -ne "DEFAULT"){$("%5C")+$SecondaryReplicaInstanceName1} ))"
+   $AGSecondaryObj = Get-Item $AGSecondaryObjPath
 
 
-    #1-2-- Peform the Planned Failover to SecondaryReplicaName1.
-    # Path of the new primary server. DEFAULT for the DEFAULT instance, replace with named instance if need be.
+   
+#Set replicas to synchronous before planned failover
         
-    Switch-SqlAvailabilityGroup `
-        -Path "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\" `
-         -ErrorAction Stop
-        #Only include the next line if it is a forced failover
-        #-AllowDataLoss -Force
-     
-     write-host "Switch Made"
-     Get-ChildItem "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\"  | Test-SqlAvailabilityReplica
-
-    #1-3-- Return secondary replica to Asynchronous
-    #Note that the values here of Primary and Secondary1 are flipped, because the variables predate the failover.
-       Set-SqlAvailabilityReplica `
-        -Path "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\$($PrimaryReplicaName)" `
-        -AvailabilityMode AsynchronousCommit `
-        -ErrorAction Stop
-       Set-SqlAvailabilityReplica `
-        -Path "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\$($SecondaryReplicaName1)" `
-        -AvailabilityMode AsynchronousCommit `
-        -ErrorAction Stop
-
-
-    #1-4 Set routing list. 
-    #MUST RUN ON NEW PRIMARY NODE
-    #You must have a URL to be part of the read-only routing. If URL's have not been specified, they must be before setting Routing List or you will get the error:
-    #An availability replica 'SQLSERVER-1' that is specified in the READ_ONLY_ROUTING_LIST for availability replica 'SQLSERVER-0' does not have a value set for READ_ONLY_ROUTING_URL.
-    #Run the below only if necessary, but they must be run from the new primary replica. Examples:
-    #Set-SqlAvailabilityReplica -ReadOnlyRoutingConnectionUrl "TCP://$($PrimaryReplicaName).contoso.com:1433" -InputObject $PrimaryReplica
-    #Set-SqlAvailabilityReplica -ReadOnlyRoutingConnectionUrl "TCP://$($SecondaryReplicaName1).contoso.com:1433" -InputObject $SecondaryReplica1
-    
     Set-SqlAvailabilityReplica `
-        -ReadOnlyRoutingList $PrimaryReplicaName,$SecondaryReplicaName1 `
-        -InputObject $SecondaryReplica1 `
-        -ErrorAction Stop
+    -Path $AGPrimaryObjPath `
+    -AvailabilityMode SynchronousCommit `
+    -FailoverMode "Manual" `
+    -ErrorAction Stop
+    Set-SqlAvailabilityReplica `
+    -Path $AGSecondaryObjPath `
+    -AvailabilityMode SynchronousCommit `
+    -FailoverMode "Manual" `
+    -ErrorAction Stop
 
+#Check for when replicas are synchronized.
+Do {
+$AGSecondaryObj.Refresh()
+$CurrentSync = ($AGSecondaryObj | Select RollupSynchronizationState | Format-Wide | Out-String).Trim()
+IF ($CurrentSync -ne "Synchronized") { 
+        Write-Output "Waiting for Synchronized state before failover, still $($CurrentSync)"
+        Start-Sleep -s 2 
+        }
+} Until ($CurrentSync -eq 'Synchronized')
 
+#Perform failover
+Write-Output "Beginning Failover $(Get-Date)"
+Switch-SqlAvailabilityGroup `
+    -Path "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\" `
+     -ErrorAction Stop `
+    #Only include the next line if it is a forced failover
+    #-AllowDataLoss -Force
+Write-Output "Failover Complete $(Get-Date)"
+Start-Sleep -s 10 #Allow failover to resolve
 
+#Return secondary replica to Asynchronous sync
+#Note that the values here of Primary and Secondary1 are flipped, because the variables predate the failover.
+Invoke-Command -script { `
+param($SecondaryReplicaName1, $SecondaryReplicaInstanceName1, $AvailabilityGroupName, $PrimaryReplicaName, $PrimaryReplicaInstanceName)
 
-#2- Forced failover
-# To be run on the new primary
+ Set-SqlAvailabilityReplica `
+-Path "SQLSERVER:\Sql\$(($SecondaryReplicaName1))\$(($SecondaryReplicaInstanceName1))\AvailabilityGroups\$(($AvailabilityGroupName))\AvailabilityReplicas\$(($SecondaryReplicaName1)+$(IF(($SecondaryReplicaInstanceName1) -ne "DEFAULT"){$("%5C")+(($SecondaryReplicaInstanceName1))} ))"  `
+-AvailabilityMode asynchronousCommit `
+-ErrorAction Stop
+    Set-SqlAvailabilityReplica `
+-Path "SQLSERVER:\Sql\$(($SecondaryReplicaName1))\$(($SecondaryReplicaInstanceName1))\AvailabilityGroups\$(($AvailabilityGroupName))\AvailabilityReplicas\$(($PrimaryReplicaName)+$(IF(($PrimaryReplicaInstanceName) -ne "DEFAULT"){$("%5C")+(($PrimaryReplicaInstanceName))} ))"  `
+-AvailabilityMode asynchronousCommit `
+-ErrorAction Stop
 
+Get-ChildItem "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\" | Test-SqlAvailabilityReplica | Format-Table
+} -ComputerName $SecondaryReplicaName1  -Args $SecondaryReplicaName1, $SecondaryReplicaInstanceName1, $AvailabilityGroupName, $PrimaryReplicaName, $PrimaryReplicaInstanceName
 
-    #Setup
-    $PrimaryReplicaName = "SQLSERVER-1"
-    $PrimaryReplicaInstanceName = "DEFAULT"
-    $SecondaryReplicaName1 = "SQLSERVER-0"
-    $SecondaryReplicaInstanceName1 = "DEFAULT"
-    $PrimaryReplica = Get-Item "AvailabilityReplicas\$($PrimaryReplicaName)"
-    $SecondaryReplica1 = Get-Item "AvailabilityReplicas\$($SecondaryReplicaName1)"
-    Set-Location "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\"
+Write-Output "End $(Get-Date)"
 
-    Get-ChildItem "SQLSERVER:\Sql\$($PrimaryReplicaName)\$($PrimaryReplicaInstanceName)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\"  | Test-SqlAvailabilityReplica
-
-    #1-1-- Peform the Planned Failover to SecondaryReplicaName1.
-    # Path of the new primary server. DEFAULT for the DEFAULT instance, replace with named instance if need be.
-        
-    Switch-SqlAvailabilityGroup `
-        -Path "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\" `
-         -ErrorAction Stop
-        #Only include the next line if it is a forced failover
-        -AllowDataLoss -Force
-     
-     write-host "Switch Made"
-     Get-ChildItem "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\"  | Test-SqlAvailabilityReplica
-
-    #1-2-- Return secondary replica to Asynchronous
-    #Note that the values here of Primary and Secondary1 are flipped, because the variables predate the failover.
-    #   Set-SqlAvailabilityReplica `
-    #    -Path "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\$($PrimaryReplicaName)" `
-    #    -AvailabilityMode AsynchronousCommit `
-    #    -ErrorAction Stop
-       Set-SqlAvailabilityReplica `
-        -Path "SQLSERVER:\Sql\$($SecondaryReplicaName1)\$($SecondaryReplicaInstanceName1)\AvailabilityGroups\$($AvailabilityGroupName)\AvailabilityReplicas\$($SecondaryReplicaName1)" `
-        -AvailabilityMode AsynchronousCommit `
-        -ErrorAction Stop
 
 
     #1-3 Set routing list. 
