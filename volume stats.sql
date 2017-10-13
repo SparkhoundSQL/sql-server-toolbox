52909445804ba7be64fb2bb642e7e2ae7bd6b3cc
william assaf <6641470+williamadba@users.noreply.github.com>
2017-10-13 16:20:38 -0500

    minor formatting

--- volume stats.sql
+++ volume stats.sql
@@ -1,15 +1,22 @@
 
 
-select distinct 
-  volume_letter = upper(vs.volume_mount_point)
+select 
+  volume_letter = UPPER(vs.volume_mount_point)
 , file_system_type
-, drive_size_GB = convert(decimal(19,2), vs.total_bytes/1024./1024./1024. )
-, drive_free_space_GB = convert(decimal(19,2), vs.available_bytes/1024./1024./1024. )
-, drive_percent_free = CONVERT(DECIMAL(5,2), vs.available_bytes * 100.0 / vs.total_bytes)
+, drive_size_GB = MAX(CONVERT(decimal(19,2), vs.total_bytes/1024./1024./1024. ))
+, drive_free_space_GB = MAX(CONVERT(decimal(19,2), vs.available_bytes/1024./1024./1024. ))
+, drive_percent_free = MAX(CONVERT(DECIMAL(5,2), vs.available_bytes * 100.0 / vs.total_bytes))
 FROM
-   sys.master_files AS f
-CROSS APPLY
+   sys.master_files AS f CROSS APPLY
    sys.dm_os_volume_stats(f.database_id, f.file_id) vs --only return volumes where there is database file (data or log)
- ORDER BY volume_letter
+ GROUP BY vs.volume_mount_point, vs.file_system_type
+ ORDER BY volume_letter 
  
 --exec xp_fixeddrives
+
+
+/*
+
+E:\	NTFS	300.00	152.94	50.98
+F:\	NTFS	300.00	84.08	28.03
+*/
