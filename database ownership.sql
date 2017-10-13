b398f147351127c5d4b155f7b325de76b46db94a
william assaf <6641470+williamadba@users.noreply.github.com>
2017-10-13 11:12:12 -0500

    add variable and samples

--- database ownership.sql
+++ database ownership.sql
@@ -1,11 +1,13 @@
+--Find database owners that are not desired
+
+declare @Desired_DB_owner varchar(255) = 'sa' --'sa' is just an example, change to desired service account, example: domain\accountname
+
 select 
-	
 	database_name = d.name
 ,	principal_name = SUSER_SNAME (d.owner_sid)
-,	set_to_sa = case when sp.name <> 'sa' THEN 'alter authorization on database::' + d.name + ' to sa' ELSE null END
-,	set_to_current = case when sp.name <> 'sa' THEN 'alter authorization on database::' + d.name + ' to [' + sp.name + ']' ELSE null END
+,	set_to_desired = 'alter authorization on database::' + d.name + ' to [' + @Desired_DB_owner + ']' 
+,	set_to_current =  case when SUSER_SNAME (d.owner_sid) <> @Desired_DB_owner THEN 'alter authorization on database::' + d.name + ' to [' + SUSER_SNAME (d.owner_sid) + ']' ELSE NULL END
 ,	* 
 from sys.databases d
-left outer join sys.server_principals sp
-on d.owner_sid = sp.sid
---where sp.name <> 'sa'
+where SUSER_SNAME (d.owner_sid) <> @Desired_DB_owner
+
--- job owners.sql
+++ job owners.sql
@@ -2,14 +2,17 @@ use msdb
 go
 --TODO Change @owner_login_name to desired SQL agent service account to own the job
 
+declare @Desired_job_owner varchar(255) = 'sa' --'sa' is just an example, change to desired service account, example: domain\accountname
+
 --sql 2005 and above
-select owner = sp.name, jobname = j.name, j.job_id
-,	change_tsql = N'EXEC msdb.dbo.sp_update_job @job_id=N'''+convert(nvarchar(100), j.job_id)+N''', @owner_login_name=N''domain\sqlsvcacct'''
-,	revert_tsql = N'EXEC msdb.dbo.sp_update_job @job_id=N'''+convert(nvarchar(100), j.job_id)+N''', @owner_login_name=N'''+sp.name+''''
+select owner = SUSER_SNAME (j.owner_sid), jobname = j.name, j.job_id
+,	change_tsql = N'EXEC msdb.dbo.sp_update_job @job_id=N'''+convert(nvarchar(100), j.job_id)+N''', @owner_login_name=N'''+@Desired_job_owner+''''
+,	revert_tsql = N'EXEC msdb.dbo.sp_update_job @job_id=N'''+convert(nvarchar(100), j.job_id)+N''', @owner_login_name=N'''+SUSER_SNAME (j.owner_sid)+''''
 from sysjobs j
 left outer join sys.server_principals  sp on j.owner_sid = sp.sid
-where sp.name not in ('sa','distributor_admin','domain\sqlsvcacct') or sp.name is null
---EXEC msdb.dbo.sp_update_job @job_id=N'd5722a5c-d7cf-4332-8af9-d5839257cb4b', @owner_login_name=N'sa'
+where 
+	(sp.name not in ('sa','distributor_admin')  and sp.name <> @Desired_job_owner)
+	or sp.name is null 
 
 /*
 --sql 2000
@@ -17,5 +20,14 @@ select sp.name, j.name, j.job_id from msdb.dbo.sysjobs j
 left outer join master.dbo.syslogins sp on j.owner_sid = sp.sid
 where sp.name not in ('sa','distributor_admin') or sp.name is null
 --EXEC msdb.dbo.sp_update_job @job_id=N'8eab379e-958e-4576-92ae-b5999aeec01c', @owner_login_name=N'distributor_admin'
+*/
+
+/*
+--Sample usage
+
+EXEC msdb.dbo.sp_update_job @job_id=N'BDAFAC9B-1705-4E47-9C26-6C4B813CB165', @owner_login_name=N'sa'
+EXEC msdb.dbo.sp_update_job @job_id=N'987AF666-A516-4847-8BA3-73DE337CFF94', @owner_login_name=N'sa'
+EXEC msdb.dbo.sp_update_job @job_id=N'AEBF4C5C-EC6D-4635-96B6-797BF4AEEC62', @owner_login_name=N'sa'
+
 */
 
