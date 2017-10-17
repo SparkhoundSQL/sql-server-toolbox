5ecb57a4872f19dc79cdc682665bda66062a6e2e
william assaf <6641470+williamadba@users.noreply.github.com>
2017-10-17 09:19:01 -0500

    add security log (requires script run as admin)

--- gather log events.ps1
+++ gather log events.ps1
@@ -9,7 +9,12 @@ $EventLog_System = Get-EventLog -LogName "System" -After (Get-Date).AddDays($Num
     ForEach-Object { $_.Group[0] | Add-Member -PassThru -NotePropertyName Count -NotePropertyValue $_.Count | Add-Member -PassThru -NotePropertyName LogSource -NotePropertyValue "System" } |
     Sort-Object Count -Descending -Unique | 
     Select-Object LogSource, Count, @{name="Latest";expression={$_.TimeGenerated}}, EventID, Source, Message ;
+$EventLog_Security = Get-EventLog -LogName "Security" -After (Get-Date).AddDays($NumDays) | 
+    ? { $_.entryType -Match "Error" -and "Critical"  } |  Group-Object -Property EventID |
+    ForEach-Object { $_.Group[0] | Add-Member -PassThru -NotePropertyName Count -NotePropertyValue $_.Count | Add-Member -PassThru -NotePropertyName LogSource -NotePropertyValue "System" } |
+    Sort-Object Count -Descending -Unique | 
+    Select-Object LogSource, Count, @{name="Latest";expression={$_.TimeGenerated}}, EventID, Source, Message ;
 
-@( $EventLog_System; $EventLog_Application) | Out-GridView;
+@( $EventLog_System; $EventLog_Application; $EventLog_Security) | Out-GridView;
 
  
