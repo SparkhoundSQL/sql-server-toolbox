57f86a8143274f417610b1067e8a45eba6e381de
william assaf <6641470+williamadba@users.noreply.github.com>
2017-11-09 18:02:58 -0600

    rename for proper git sorting

--- Unsent_DB_Email.sql
+++
@@ -1,8 +0,0 @@
-SELECT m.recipients, m.subject, m.body, 
-    m.send_request_date, m.send_request_user, m.sent_status
-FROM msdb.dbo.sysmail_allitems m
-WHERE m.sent_status<>'sent'
---WHERE m.send_request_date > CURRENT_TIMESTAMP - 3
---AND m.sent_status <> 'sent'
-ORDER BY m.send_request_date DESC
-GO
