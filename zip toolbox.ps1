e688ae9e723d97b45546c1987802502a4c0b8c1e
william assaf <6641470+williamadba@users.noreply.github.com>
2018-01-05 11:52:08 -0600

    update zip process to exclude git files for cleaner download

--- zip toolbox.ps1
+++ zip toolbox.ps1
@@ -1,5 +1,5 @@
-cd E:\OneDrive\toolbox
-Compress-Archive -Path * -DestinationPath toolbox.zip -Force
-
 #There is a Windows Scheduled task on my machine that runs
 #PowerShell -File "E:\OneDrive\toolbox\zip toolbox.ps1"
+
+cd E:\OneDrive\toolbox
+get-childitem .\* -Recurse | Where-Object {$_.FullName -notlike '*\.git*' }  | Where-Object {$_.FullName -notlike "*toolbox.zip" } | Compress-Archive -DestinationPath .\toolbox.zip -Force 
