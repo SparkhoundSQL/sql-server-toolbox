5c31a12877fb80eb6a7a3041254b9e823db6266f
william assaf <6641470+williamadba@users.noreply.github.com>
2018-05-31 10:39:23 -0500

    updates to comment

--- my_permissions.sql
+++ my_permissions.sql
@@ -1,16 +1,18 @@
 select suser_sname(); --you
-execute as login = 'whateverloginname' --login name you want to test
+execute as login = 'sparkhound\test.user' --login name you want to test
 
-use Labor_Mobile_Labor
+use master
 select suser_sname(), * from sys.fn_my_permissions (null, 'DATABASE') --https://msdn.microsoft.com/en-us/library/ms176097.aspx
+
 REVERT; --undo the execute as
 select suser_sname(); --you
 
-use Labor_Mobile_Labor
---GRANT EXECUTE  to whateverusername --granting database permissions here to user. Typically, this shares the same name as the Login, but not necessarily.
+use master
+--GRANT EXECUTE  to [sparkhound\test.user] --granting database permissions here to user. Typically, this shares the same name as the Login, but not necessarily.
 
 
---Get members of a security group from within SQL
+--Get members of a windows security group from within SQL
+--Returns members but NOT subgroups! There isn't a way in SQL to see subgroups.
 EXEC master..xp_logininfo 
-@acctname = '[group]',
+@acctname = 'domain\groupname',
 @option = 'members'
