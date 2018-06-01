aca90cbeae6aebbc327735e62722af419888105d
william assaf <6641470+williamadba@users.noreply.github.com>
2018-06-01 16:56:14 -0500

    initial checkin of this script

---
+++ oledb providers.sql
@@ -0,0 +1,17 @@
+ --OLE DB Providers registered to this server
+ EXEC master.dbo.sp_MSset_oledb_prop 
+
+ --DB2OLEDB = Microsoft-provided as400 OLE DB driver for AS400 as found in SQL Server version-specific feature pack 
+ --Microsoft.ACE.OLEDB.12.0 = MS Access
+
+ --Default list
+
+--SQLOLEDB
+--SQLNCLI11
+--Microsoft.ACE.OLEDB.12.0
+--ADsDSOObject
+--SSISOLEDB
+--Search.CollatorDSO
+--MSDASQL
+--MSOLAP
+--MSDAOSP
