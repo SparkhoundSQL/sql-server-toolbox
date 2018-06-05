54620e9f5498682be58e932d7818e640c3b5e817
william assaf <6641470+williamadba@users.noreply.github.com>
2018-06-05 16:22:40 -0500

    fix potential typo- wrap db name in brackets

--- database ownership.sql
+++ database ownership.sql
@@ -5,9 +5,10 @@ declare @Desired_DB_owner varchar(255) = 'sa' --'sa' is just an example, change 
 select 
 	database_name = d.name
 ,	principal_name = SUSER_SNAME (d.owner_sid)
-,	set_to_desired = 'alter authorization on database::' + d.name + ' to [' + @Desired_DB_owner + ']' 
-,	set_to_current =  case when SUSER_SNAME (d.owner_sid) <> @Desired_DB_owner THEN 'alter authorization on database::' + d.name + ' to [' + SUSER_SNAME (d.owner_sid) + ']' ELSE NULL END
+,	set_to_desired = 'alter authorization on database::[' + d.name + '] to [' + @Desired_DB_owner + ']' 
+,	set_to_current =  case when SUSER_SNAME (d.owner_sid) <> @Desired_DB_owner THEN 'alter authorization on database::[' + d.name + '] to [' + SUSER_SNAME (d.owner_sid) + ']' ELSE NULL END
 ,	* 
 from sys.databases d
 where SUSER_SNAME (d.owner_sid) <> @Desired_DB_owner
+
 
