83da8f7e74bcd32507084a1216cb08fab034e2f5
william assaf <6641470+williamadba@users.noreply.github.com>
2018-06-22 13:21:26 -0500

    better column aliases

--- defrag columnstore.sql
+++ defrag columnstore.sql
@@ -2,15 +2,29 @@
 --https://docs.microsoft.com/en-us/sql/relational-databases/indexes/columnstore-indexes-defragmentation
 
 SELECT 
-SCHEMA_NAME (gps.object_id), object_name(gps.object_id), i.name, RowGroup_count = count(gps.row_group_id) , gps.state_desc, RowGroup_rows = sum(gps.total_rows) ,
-CASE WHEN state_desc = 'open' and count(row_group_id) > 1 and sum(total_rows)>0 THEN 
+	TableName = SCHEMA_NAME (o.schema_id) + '.' + o.name
+,	IndexName = i.name
+,	RowGroup_count = count(gps.row_group_id) , RowGroup_State = gps.state_desc, RowGroup_rows = sum(gps.total_rows)
+,	Rebuild_TSQL_if_needed = CASE WHEN state_desc = 'open' and count(row_group_id) > 1 and sum(total_rows)>0 THEN 
 'ALTER INDEX '+i.name+' ON '+object_name(gps.object_id)+' REORGANIZE WITH (COMPRESS_ALL_ROW_GROUPS = ON);   
 ALTER INDEX '+i.name+' ON '+object_name(gps.object_id)+' REORGANIZE; 
 ALTER INDEX '+i.name+' ON '+object_name(gps.object_id)+' REORGANIZE; 
---Consolidate the Open rowgroups with COMPRESS_ALL_ROW_GROUPS, then again to compress the COMPRESSED rowgroups, then a third time to remove the TOMBSTONE rowgroups'
+--Consolidate the Open rowgroups with COMPRESS_ALL_ROW_GROUPS, 
+--then again to compress the COMPRESSED rowgroups,
+--then a third time to remove the TOMBSTONE rowgroups'
 ELSE '' END
+,	Rebuild_TSQL =				'ALTER INDEX '+i.name+' ON '+object_name(gps.object_id)+' REORGANIZE WITH (COMPRESS_ALL_ROW_GROUPS = ON);   
+ALTER INDEX '+i.name+' ON '+object_name(gps.object_id)+' REORGANIZE; 
+ALTER INDEX '+i.name+' ON '+object_name(gps.object_id)+' REORGANIZE; 
+--Consolidate the Open rowgroups with COMPRESS_ALL_ROW_GROUPS, 
+--then again to compress the COMPRESSED rowgroups,
+--then a third time to remove the TOMBSTONE rowgroups'
+								
 FROM sys.dm_db_column_store_row_group_physical_stats as gps
 INNER JOIN sys.indexes i on gps.object_id = i.object_id and gps.index_id = i.index_id
+INNER JOIN sys.objects o on i.object_id = o.object_id 
 --WHERE object_name(gps.object_id)= 'DimShipTo'
-GROUP BY i.name, gps.state_desc, gps.object_id
+GROUP BY o.name, gps.state_desc, gps.object_id, o.schema_id, i.name
+ORDER BY o.name, i.name
 GO
+
