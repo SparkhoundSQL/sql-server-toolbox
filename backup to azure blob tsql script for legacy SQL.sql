36f09918dfcb37e6120c143cc29eabc8b8e48273
william assaf <6641470+williamadba@users.noreply.github.com>
2018-07-30 15:10:50 -0500

    prep for generic use

--- backup to azure blob tsql script for legacy SQL.sql
+++ backup to azure blob tsql script for legacy SQL.sql
@@ -1,12 +1,12 @@
 --https://docs.microsoft.com/en-us/sql/relational-databases/backup-restore/sql-server-backup-to-url?view=sql-server-2017
 
-DECLARE @DB_name VARCHAR(50) -- database name 
-DECLARE @BackupLoc VARCHAR(256) -- path for backup files 
-DECLARE @BackupfileName VARCHAR(256) -- filename for backup 
+DECLARE @DB_name VARCHAR(256) -- database name 
+DECLARE @BackupLoc VARCHAR(512) -- path for backup files 
+DECLARE @BackupfileName VARCHAR(512) -- filename for backup 
 DECLARE @fileDate VARCHAR(20) -- used for file name
 DECLARE @process VARCHAR(2000) -- used for documentation
 -- specify database backup container location
-SET @BackupLoc = 'https://sphsqlbackup.blob.core.windows.net/prodsqlbak/sh-sp2013-sql/' 
+SET @BackupLoc = 'https://whatever.blob.core.windows.net/prodsqlbak/sh-sp2013-sql/' 
 
 DECLARE db_cursor CURSOR FOR 
 SELECT name FROM master.sys.databases WHERE database_id > 4 and state=0 and is_read_only = 0
@@ -16,24 +16,24 @@ WHILE @@FETCH_STATUS = 0
 BEGIN  
 	set @fileDate= replace(replace(convert(nvarchar(50),getdate()),' ','_'),':','_')
 	SET @BackupfileName = @BackupLoc + @DB_name + '_' + @fileDate + '.DIF' 
-    SET @process = 'BACKUP DATABASE '+@DB_name+ ' TO URL ='''+ @BackupfileName  +''' WITH CREDENTIAL = ''https://sphsqlbackup.blob.core.windows.net/prodsqlbak''
-		, DIFFERENTIAL
-		, COMPRESSION, CHECKSUM, FORMAT, BLOCKSIZE=65536, MAXTRANSFERSIZE=4194304;'
-	print @process
+    SET @process = 'BACKUP DATABASE '+@DB_name+ ' TO URL ='''+ @BackupfileName  +''' WITH CREDENTIAL = ''https://whatever.blob.core.windows.net/prodsqlbak''
+		, COMPRESSION, CHECKSUM, FORMAT;'
+	--print @process
 	BEGIN TRY
        BACKUP DATABASE @DB_name TO URL = @BackupfileName  
-		WITH CREDENTIAL = N'https://sphsqlbackup.blob.core.windows.net/prodsqlbak'
-		, DIFFERENTIAL
-		, COMPRESSION, CHECKSUM, FORMAT, BLOCKSIZE=65536, MAXTRANSFERSIZE=4194304;
+		WITH CREDENTIAL = N'https://whatever.blob.core.windows.net/prodsqlbak'
+		, COMPRESSION, CHECKSUM, FORMAT;
     END TRY
 	BEGIN CATCH
-		INSERT INTO DBALogging.dbo.errortable ([ErrorNumber], [ErrorSeverity], [ErrorState], [ErrorProcedure], [ErrorLine], [ErrorMessage], [Process])
-		SELECT ERROR_NUMBER() AS ErrorNumber, ERROR_SEVERITY() AS ErrorSeverity, ERROR_STATE() as ErrorState, ERROR_PROCEDURE() as ErrorProcedure, ERROR_LINE() as ErrorLine, ERROR_MESSAGE() as ErrorMessage,
-			Process = @process;
+		--Only captures the 3013, not the preceding and actual error message for any backup failure. :(
+		--INSERT INTO DBALogging.dbo.errortable ([ErrorNumber], [ErrorSeverity], [ErrorState], [ErrorProcedure], [ErrorLine], [ErrorMessage], [Process])
+		--SELECT ERROR_NUMBER() AS ErrorNumber, ERROR_SEVERITY() AS ErrorSeverity, ERROR_STATE() as ErrorState, ERROR_PROCEDURE() as ErrorProcedure, ERROR_LINE() as ErrorLine, ERROR_MESSAGE() as ErrorMessage
+		--	, Process = 'Testing'; --need the semicolon
+	
+		THROW --optional, to actually cause a failure. 
+
 	END CATCH
-
-	WAITFOR DELAY '00:01' -- one minute delay between to not exhaust Azure and create error 3013 errors
-
+		
 	FETCH NEXT FROM db_cursor INTO @DB_name  
 
 END  
