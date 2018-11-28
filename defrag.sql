b59f2a80c2b8a1ec5df791a3b4eab00512d5448c
william assaf <6641470+williamadba@users.noreply.github.com>
2018-11-28 09:51:36 -0600

    restructure, add comments and references, reformat

--- defrag.sql
+++ defrag.sql
@@ -1,52 +1,50 @@
-USE w 
-go
---Using ALTER INDEX ALL isn't efficient. Instead, consider index-level or index-partition-level REBUILDs.
---See also \toolbox\automated index rebuild w online 2016.sql
---See also \toolbox\defrag columnstore.sql
+--BEWARE running this script on production can harm performance by hammering IO.
+--Run in each database
 
---Consider also DATA_COMPRESSION, SORT_IN_TEMPDB, ONLINE, MAXDOP options
+--Using ALTER INDEX ALL as in this example isn't efficient. This script averages fragmentation for all indexes/partitions on a table. Instead, consider index-level or index-partition-level REBUILDs.
+
+--See also \toolbox\automated index rebuild.sql
+--See also \toolbox\defrag columnstore.sql
+--See also \toolbox\tables without clustered indexes.sql
+
+--Consider also DATA_COMPRESSION, ONLINE, MAXDOP options
+--	WITH (MAXDOP = 1, ONLINE = ON);
 
 SELECT DISTINCT
-	SQL_Reorg = 'ALTER INDEX ALL ON ' + x.DB + '.' + x.[schema_name] + '.' + x.[table_name] + ' REORGANIZE; 
-		UPDATE STATISTICS ' + x.DB + '.' + x.[schema_name] + '.' + x.[table_name]  + ';'
-,	SQL_Rebuild = 'ALTER INDEX ALL ON ' + x.DB + '.' + x.[schema_name] + '.' + x.[table_name] + ' REBUILD;'  
-,	avg_fragmentation_pct	=	avg(avg_fragmentation_pct)
-,	page_count	=	sum(page_count)
---, * 
-FROM 
-(
-select 
-  DB = db_name(s.database_id)
-, [schema_name] = sc.name
-, [table_name] = o.name
-, index_name = i.name
-, s.index_type_desc
-, s.partition_number
-, avg_fragmentation_pct = s.avg_fragmentation_in_percent
-, s.page_count
-from sys.indexes as i 
-CROSS APPLY sys.dm_db_index_physical_stats (DB_ID(),i.object_id,i.index_id, null,'limited') as s
-INNER JOIN sys.objects as o ON o.object_id = s.object_id
-INNER JOIN sys.schemas as sc ON o.schema_id = sc.schema_id
-WHERE i.is_disabled = 0
---AND s.page_count > 12800 --12800 pages is 100mb
-AND	alloc_unit_type_desc <> 'LOB_DATA'
+	SQL_Reorg				= 'ALTER INDEX ALL ON ' + x.DB + '.' + x.[schema_name] + '.' + x.[table_name] + ' REORGANIZE; 
+								UPDATE STATISTICS ' + x.DB + '.' + x.[schema_name] + '.' + x.[table_name]  + ';'
+,	SQL_Rebuild				= 'ALTER INDEX ALL ON ' + x.DB + '.' + x.[schema_name] + '.' + x.[table_name] + ' REBUILD WITH (SORT_IN_TEMPDB = ON);'  
+,	max_fragmentation_pct	=	MAX(avg_fragmentation_pct) -- Use the highest amount of fragmentation we find in any index on a table.
+,	avg_fragmentation_pct	=	AVG(avg_fragmentation_pct) -- Use the avg amount of fragmentation we find across all indexes 
+,	page_count				=	SUM(page_count)
+,	number_of_indexes		= COUNT(index_id) 
+FROM (	SELECT 
+		  DB = db_name(s.database_id)
+		, [schema_name] = sc.name
+		, [table_name] = o.name
+		, index_name = i.name
+		, s.index_type_desc
+		, s.partition_number
+		, avg_fragmentation_pct = s.avg_fragmentation_in_percent
+		, s.page_count
+		, i.index_id
+		from sys.indexes as i 
+		CROSS APPLY sys.dm_db_index_physical_stats (DB_ID(),i.object_id,i.index_id, null,'limited') as s
+		INNER JOIN sys.objects as o ON o.object_id = s.object_id
+		INNER JOIN sys.schemas as sc ON o.schema_id = sc.schema_id
+		WHERE i.is_disabled = 0
+		AND	alloc_unit_type_desc <> 'LOB_DATA'
+		AND o.object_id not in (select object_id from sys.indexes where index_id = 0) -- This table is a heap and probably needs a clustered index. Rebuilding will do no good. Ignore. 
+	) x
+GROUP BY x.DB , x.[schema_name] , x.[table_name] 
 
-) x
---WHERE avg_fragmentation_pct > 70
-group by x.DB , x.[schema_name] , x.[table_name]
-order by page_count desc, avg_fragmentation_pct desc
+HAVING 1=1
+AND	SUM(page_count) > 1280 --1280 pages is 10mb, ignore anything smaller
+AND AVG(avg_fragmentation_pct) > 50
 
-GO
+ORDER BY SUM(page_count) desc, AVG(avg_fragmentation_pct) desc;
 
 /*
---Consider also adding
-WITH (MAXDOP = 1, ONLINE = ON);
 
-
-ALTER INDEX ALL ON w.dbo.ActivateTable_guid REORGANIZE; 
-		UPDATE STATISTICS w.dbo.ActivateTable_guid;
-ALTER INDEX ALL ON w.dbo.fragmented_table REORGANIZE; 
-		UPDATE STATISTICS w.dbo.fragmented_table;
 
 */
