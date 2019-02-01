69ad4c5ba94afa1e1f0b1189769954855cae00e0
william assaf <6641470+williamadba@users.noreply.github.com>
2019-02-01 14:02:14 -0600

    add sp_MSforeachdb to scan db's, change join order to find unused catalogs

--- fulltext index status.sql
+++ fulltext index status.sql
@@ -1,8 +1,15 @@
-
+--Identify if fulltext catalog feature is installed
+--Skip this step in Azure SQL DB
 IF (SELECT FullText_Indexing_Is_Installed = fulltextserviceproperty('IsFullTextInstalled')) <> 1
 BEGIN
 	THROW 51000, 'Full text indexing is not installed.',0;
 END;
+GO
+
+--Identify databases with a fulltext catalog present
+--Skip this step in Azure SQL DB
+EXEC sp_MSforeachdb 'use[?]; select Database_name = DB_Name(), fc.name from sys.fulltext_catalogs fc'
+GO
 
 SELECT	
 		Fulltext_Catalog	= c.name
@@ -31,10 +38,10 @@ SELECT
 	,	fi.has_crawl_completed
 	,	c.is_importing -- Indicates whether the full-text catalog is being imported: 1 = The catalog is being imported. 2 = The catalog is not being imported.
 	
-FROM	sys.fulltext_indexes fi
-    INNER JOIN sys.fulltext_catalogs c	ON fi.fulltext_catalog_id = c.fulltext_catalog_id
-	INNER JOIN sys.objects o			ON o.[object_id] = fi.[object_id] 
-    INNER JOIN sys.indexes i			ON fi.unique_index_id = i.index_id AND fi.[object_id] = i.[object_id]
+FROM	sys.fulltext_catalogs c	
+    LEFT OUTER JOIN sys.fulltext_indexes fi ON fi.fulltext_catalog_id = c.fulltext_catalog_id
+	LEFT OUTER JOIN sys.objects o			ON o.[object_id] = fi.[object_id] 
+    LEFT OUTER JOIN sys.indexes i			ON fi.unique_index_id = i.index_id AND fi.[object_id] = i.[object_id]
 /*
 WHERE  (fi.crawl_end_date is null --is currently crawling
   or fi.crawl_end_date < dateadd(day, -1, getdate())) --look for any ft index that hasn't updated recently
