5e6caf5eb2ebef2f04558da3d4dc875e2deb798c
william assaf <6641470+williamadba@users.noreply.github.com>
2019-02-01 11:07:10 -0600

    formatting

--- azure 15min dtu usage.sql
+++ azure 15min dtu usage.sql
@@ -1,46 +1,43 @@
 --https://msdn.microsoft.com/en-us/library/dn800981.aspx
 --Run in the master Azure SQL server database
+SELECT SYSDATETIMEOFFSET()
 
-select 
-	Database_Name
-,	sku --Basic, Standard, Premium
-,	TierDTU	= dtu_limit
-,	Storage_in_mb			=	MAX(Storage_in_megabytes)
-,	'Average CPU Utilization In %'			=	AVG(avg_cpu_percent)			
-,	'Maximum CPU Utilization In %'			=	MAX(avg_cpu_percent)			
-,	'Average Data IO In %'					=	AVG(avg_data_io_percent)		
-,	'Maximum Data IO In %'					=	MAX(avg_data_io_percent)		
-,	'Average Log Write Utilization In %'	=	AVG(avg_log_write_percent)		   
-,	'Maximum Log Write Utilization In %'	=	MAX(avg_log_write_percent)		   
-,	'Average Requests In %'					=	AVG(max_worker_percent)	
-,	'Maximum Requests In %'					=	MAX(max_worker_percent)	
-,	'Average Sessions In %'					=	AVG(max_session_percent)	
-,	'Maximum Sessions In %'					=	MAX(max_session_percent)	
---select *
-from 
-master.sys.resource_stats as rs  --past 14 days 
-group by Database_Name, sku, dtu_limit
-order by Database_Name desc
+SELECT 
+	rs.Database_Name
+,	rs.sku --Basic, Standard, Premium
+,	TierDTU									= rs.dtu_limit
+,	Storage_in_mb							=	MAX(rs.Storage_in_megabytes)
+,	'Average CPU Utilization In %'			=	AVG(rs.avg_cpu_percent)			
+,	'Maximum CPU Utilization In %'			=	MAX(rs.avg_cpu_percent)			
+,	'Average Data IO In %'					=	AVG(rs.avg_data_io_percent)		
+,	'Maximum Data IO In %'					=	MAX(rs.avg_data_io_percent)		
+,	'Average Log Write Utilization In %'	=	AVG(rs.avg_log_write_percent)		   
+,	'Maximum Log Write Utilization In %'	=	MAX(rs.avg_log_write_percent)		   
+,	'Average Requests In %'					=	AVG(rs.max_worker_percent)	
+,	'Maximum Requests In %'					=	MAX(rs.max_worker_percent)	
+,	'Average Sessions In %'					=	AVG(rs.max_session_percent)	
+,	'Maximum Sessions In %'					=	MAX(rs.max_session_percent)	
+FROM  master.sys.resource_stats as rs  --past 14 days 
+GROUP BY rs.Database_Name, rs.sku, rs.dtu_limit
+ORDER BY rs.Database_Name desc
 
-select 
+SELECT 
 	Timestamp				=	datetimefromparts(year(rs.end_time), month(rs.end_time), day(rs.end_time), datepart(hh,rs.end_time), datepart(minute, rs.end_time),0,0)
-,	Database_Name
-,	sku --Basic, Standard, Premium
-,	TierDTU					= dtu_limit
-,	Storage_in_mb			=	MAX(Storage_in_megabytes)
-,	'Average CPU Utilization In %'			=	AVG(avg_cpu_percent)			
-,	'Maximum CPU Utilization In %'			=	MAX(avg_cpu_percent)			
-,	'Average Data IO In %'					=	AVG(avg_data_io_percent)		
-,	'Maximum Data IO In %'					=	MAX(avg_data_io_percent)		
-,	'Average Log Write Utilization In %'	=	AVG(avg_log_write_percent)		   
-,	'Maximum Log Write Utilization In %'	=	MAX(avg_log_write_percent)		   
-,	'Average Worker Requests In %'					=	AVG(max_worker_percent)	
-,	'Maximum Worker Requests In %'					=	MAX(max_worker_percent)	
-,	'Average Sessions In %'					=	AVG(max_session_percent)	
-,	'Maximum Sessions In %'					=	MAX(max_session_percent)	
---select *
-from 
-master.sys.resource_stats as rs  --past 14 days only
-group by Database_Name, sku, dtu_limit, datetimefromparts(year(rs.end_time), month(rs.end_time), day(rs.end_time), datepart(hh,rs.end_time), datepart(minute, rs.end_time),0,0)
-order by Database_Name desc, TimeStamp DESC 
+,	rs.Database_Name
+,	rs.sku --Basic, Standard, Premium
+,	TierDTU									=	rs.dtu_limit
+,	Storage_in_mb							=	MAX(rs.Storage_in_megabytes)
+,	'Average CPU Utilization In %'			=	AVG(rs.avg_cpu_percent)			
+,	'Maximum CPU Utilization In %'			=	MAX(rs.avg_cpu_percent)			
+,	'Average Data IO In %'					=	AVG(rs.avg_data_io_percent)		
+,	'Maximum Data IO In %'					=	MAX(rs.avg_data_io_percent)		
+,	'Average Log Write Utilization In %'	=	AVG(rs.avg_log_write_percent)		   
+,	'Maximum Log Write Utilization In %'	=	MAX(rs.avg_log_write_percent)		   
+,	'Average Worker Requests In %'			=	AVG(rs.max_worker_percent)	
+,	'Maximum Worker Requests In %'			=	MAX(rs.max_worker_percent)	
+,	'Average Sessions In %'					=	AVG(rs.max_session_percent)	
+,	'Maximum Sessions In %'					=	MAX(rs.max_session_percent)	
+FROM master.sys.resource_stats as rs  --past 14 days only
+GROUP BY rs.Database_Name, rs.sku, rs.dtu_limit, datetimefromparts(year(rs.end_time), month(rs.end_time), day(rs.end_time), datepart(hh,rs.end_time), datepart(minute, rs.end_time),0,0)
+ORDER BY rs.Database_Name desc, TimeStamp DESC 
 
--- azure 15sec dtu usage.sql
+++ azure 15sec dtu usage.sql
@@ -1,34 +1,36 @@
 --https://msdn.microsoft.com/en-us/library/dn800981.aspx
 --Run in the user Azure database
+SELECT SYSDATETIMEOFFSET()
+GO
 
 SELECT  
 	Database_Name = DB_NAME()
-,	TierDTU	= dtu_limit
-,	'Average CPU Utilization In %'			=	AVG(avg_cpu_percent)			
-,	'Maximum CPU Utilization In %'			=	MAX(avg_cpu_percent)			
-,	'Average Data IO In %'					=	AVG(avg_data_io_percent)		
-,	'Maximum Data IO In %'					=	MAX(avg_data_io_percent)		
-,	'Average Log Write Utilization In %'	=	AVG(avg_log_write_percent)		   
-,	'Maximum Log Write Utilization In %'	=	MAX(avg_log_write_percent)		
-,   'Average Memory Usage In %'				=	AVG(avg_memory_usage_percent)  
-,   'Maximum Memory Usage In %'   			=	MAX(avg_memory_usage_percent)  
-FROM sys.dm_db_resource_stats  --past hour only
-group by dtu_limit
+,	TierDTU	= rs.dtu_limit
+,	'Average CPU Utilization In %'				=	AVG(rs.avg_cpu_percent)			
+,	'Maximum CPU Utilization In %'				=	MAX(rs.avg_cpu_percent)			
+,	'Average Data IO In %'						=	AVG(rs.avg_data_io_percent)		
+,	'Maximum Data IO In %'						=	MAX(rs.avg_data_io_percent)		
+,	'Average Log Write Utilization In %'		=	AVG(rs.avg_log_write_percent)		   
+,	'Maximum Log Write Utilization In %'		=	MAX(rs.avg_log_write_percent)		
+,   'Average Memory Usage In %'					=	AVG(rs.avg_memory_usage_percent)  
+,   'Maximum Memory Usage In %'   				=	MAX(rs.avg_memory_usage_percent)  
+FROM sys.dm_db_resource_stats as rs  --past hour only
+group by rs.dtu_limit
 
-go
+GO
 select 
-	UTC_time = end_time
-,	'CPU Utilization In % of Limit'  	    = rs.avg_cpu_percent
-,	'Data IO In % of Limit'					= rs.avg_data_io_percent
-,	'Log Write Utilization In % of Limit'	= rs.avg_log_write_percent
-,	'Memory Usage In % of Limit'			= rs.avg_memory_usage_percent 
-,	'In-Memory OLTP Storage in % of Limit'	= rs.xtp_storage_percent
-,	'Concurrent Worker Threads in % of Limit'		= rs.max_worker_percent
-,	'Concurrent Sessions in % of Limit'		= rs.max_session_percent
-
+	Database_Name = DB_NAME()
+,	UTC_time = end_time
+,	'CPU Utilization In % of Limit'  			=	rs.avg_cpu_percent
+,	'Data IO In % of Limit'						=	rs.avg_data_io_percent
+,	'Log Write Utilization In % of Limit'		=	rs.avg_log_write_percent
+,	'Memory Usage In % of Limit'				=	rs.avg_memory_usage_percent 
+,	'In-Memory OLTP Storage in % of Limit'		=	rs.xtp_storage_percent
+,	'Concurrent Worker Threads in % of Limit'	=	rs.max_worker_percent
+,	'Concurrent Sessions in % of Limit'			=	rs.max_session_percent
 from 
 sys.dm_db_resource_stats as rs  --past hour only
-order by end_time desc
+order by rs.end_time desc
 
 
   
