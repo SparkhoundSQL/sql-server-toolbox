a0bad69bc5b172d84a25d1b12134bff4f44decc9
David <david.nguyen@sparkhound.com>
2019-03-13 13:13:43 -0500

    Update database mail diag.sql
    Added table dbo.sysmail_account and column name to identify which database mail profile sent

--- database mail diag.sql
+++ database mail diag.sql
@@ -7,13 +7,15 @@ GO
 
 --Find recent unsent emails
 SELECT m.send_request_date, m.recipients, m.copy_recipients, m.blind_copy_recipients
-, m.[subject], m.send_request_user, m.sent_status
+, m.[subject],  a.name AS sent_account, m.send_request_user, m.sent_status
 FROM msdb.dbo.sysmail_allitems m
-WHERE	m.send_request_date > dateadd(day, -3, sysdatetime()) -- Only show recent day(s)
-AND		m.sent_status<>'sent' -- Possible values are sent (successful), unsent (in process), retrying (failed but retrying), failed (no longer retrying)
+LEFT JOIN msdb.dbo.sysmail_account a
+	ON m.sent_account_id = a.account_id
+WHERE	1=1
+--AND m.send_request_date > dateadd(day, -3, sysdatetime()) -- Only show recent day(s), comment out if wanting to look at all mail sent
+--AND m.sent_status<>'sent' -- Possible values are sent (successful), unsent (in process), retrying (failed but retrying), failed (no longer retrying)
 ORDER BY m.send_request_date DESC;
 GO
-
 
 --Send mail test
 --exec msdb.dbo.sp_send_dbmail @profile_name ='hotmail', @recipients ='williamdassaf@hotmail.com', @subject ='test', @body = 'test'
