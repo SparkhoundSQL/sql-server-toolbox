6b23920e8b7687a716e2237cf096ff2a911b868b
william assaf <6641470+williamadba@users.noreply.github.com>
2019-03-13 14:43:05 -0500

    Update database mail diag.sql

--- database mail diag.sql
+++ database mail diag.sql
@@ -1,19 +1,19 @@
-SELECT is_broker_enabled FROM sys.databases WHERE name = 'msdb' ;
-
-EXECUTE msdb.dbo.sysmail_help_status_sp ;
+SELECT is_broker_enabled FROM sys.databases WHERE name = 'msdb' ; -- should be 1
+EXECUTE msdb.dbo.sysmail_help_status_sp ; --should say STARTED
 --EXECUTE msdb.dbo.sysmail_start_sp --start the database mail queues;
-
 GO
 
---Find recent unsent emails
+--Find recent unsent emails, hopefully there are none
 SELECT m.send_request_date, m.recipients, m.copy_recipients, m.blind_copy_recipients
 , m.[subject], m.send_request_user, m.sent_status
+, Error_Description = l.description 
 FROM msdb.dbo.sysmail_allitems m
-WHERE	m.send_request_date > dateadd(day, -3, sysdatetime()) -- Only show recent day(s)
-AND		m.sent_status<>'sent' -- Possible values are sent (successful), unsent (in process), retrying (failed but retrying), failed (no longer retrying)
+INNER JOIN msdb.dbo.sysmail_event_log AS l  
+    ON m.mailitem_id = l.mailitem_id  
+WHERE	m.send_request_date > dateadd(day, -45, sysdatetime()) -- Only show recent day(s)
+AND		m.sent_status <> 'sent' -- Possible values are sent (successful), unsent (in process), retrying (failed but retrying), failed (no longer retrying)
 ORDER BY m.send_request_date DESC;
 GO
-
 
 --Send mail test
 --exec msdb.dbo.sp_send_dbmail @profile_name ='hotmail', @recipients ='williamdassaf@hotmail.com', @subject ='test', @body = 'test'
