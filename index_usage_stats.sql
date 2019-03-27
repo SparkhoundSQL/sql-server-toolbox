5d640b7a4272c151746b09cb98eb3ca60ed140c8
william assaf <6641470+williamadba@users.noreply.github.com>
2019-03-27 12:05:50 -0500

    improved comments, partition handling, filtering

--- index_usage_stats.sql
+++ index_usage_stats.sql
@@ -1,42 +1,55 @@
+--Discover indexes that aren't helping reads but still hurting writes
+--Does not show tables that have never been written to
 
-
-SELECT  [tSQL]				= 'DROP INDEX [' + i.name + '] ON [' + sc.name + '].[' + o.name + ']'
-	,	o.name
+SELECT  TableName 			= ' [' + sc.name + '].[' + o.name + ']'
     ,   IndexName			= i.name
     ,   s.user_seeks
     ,   s.user_scans
     ,   s.user_lookups
     ,   s.user_updates
 	,	ps.row_count
-	,	SizeMb				= (ps.in_row_reserved_page_count*8.)/1024.
+	,	SizeMb				= cast((ps.in_row_reserved_page_count*8.)/1024. as decimal(19,2))
 	,	s.last_user_lookup
 	,	s.last_user_scan
 	,	s.last_user_seek
 	,	s.last_user_update
-FROM sys.dm_db_index_usage_stats s 
-         INNER JOIN sys.indexes i
-           ON i.object_id = s.object_id
-              AND i.index_id = s.index_id
-		  INNER JOIN sys.objects o
-			 ON o.object_id=i.object_id
+	,	Partition_Schema_Name = psch.[name]
+	,	Partition_Number = pr.partition_number
+	,	[tSQL]	= '--caution! DROP INDEX [' + i.name + '] ON [' + sc.name + '].[' + o.name + ']' --caution!!
+--select object_name(object_id), * 
+FROM	sys.dm_db_index_usage_stats s 
+        INNER JOIN sys.objects o
+			 ON o.object_id=s.object_id
 		inner join sys.schemas sc
 			on sc.schema_id = o.schema_id
-		inner join sys.partitions pr 
+		INNER JOIN sys.indexes i
+           ON i.object_id = s.object_id
+              AND i.index_id = s.index_id
+		left outer join sys.partitions pr 
 			on pr.object_id = i.object_id 
 			and pr.index_id = i.index_id
-		inner join sys.dm_db_partition_stats ps
+		left outer join sys.dm_db_partition_stats ps
 			on ps.object_id = i.object_id
 			and ps.partition_id = pr.partition_id
-WHERE    o.is_ms_shipped = 0
---and o.type_desc = 'USER_TABLE'
+		left outer join sys.partition_schemes psch 
+			on psch.data_space_id = i.data_space_id
+WHERE 1=1 
+--Strongly recommended filters
+and o.is_ms_shipped = 0
+and o.type_desc = 'USER_TABLE'
 and i.type_desc = 'NONCLUSTERED'
---and user_updates / 5. > (user_seeks + user_scans + user_lookups )
---and o.name in ('ContactBase')
---and o.name not like '%cascade%'
---order by [OBJECT NAME]
 and is_unique = 0
 and is_primary_key = 0
 and is_unique_constraint = 0
+
+--Optional filters
+--and user_updates / 50. > (user_seeks + user_scans + user_lookups ) --arbitrary
+--and o.name in ('ContactBase')
+--and o.name not like '%cascade%'
 --and (ps.in_row_reserved_page_count) > 1280 --10mb
-order by user_seeks + user_scans + user_lookups  asc,  s.user_updates desc
+
+
+--order by Tablename, indexname
+order by user_seeks + user_scans + user_lookups  asc,  s.user_updates desc;
+
 
