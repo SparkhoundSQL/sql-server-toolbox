d81a27694a234e158fd2a5c94dce516b71fe3cd6
william assaf <6641470+williamadba@users.noreply.github.com>
2019-09-27 12:07:10 -0500

    updates to backup history
    for comments, add AG awareness to backup history, add check for backups to NUL

--- backup history.sql
+++ backup history.sql
@@ -1,72 +1,64 @@
+--Looks for a complete backup history, displaying the latest backup of each type.
+
+--See also toolbox/multiserver backup history.sql for health checks.
+
 use master
 go
---sql2005 and above
-
---Look for gaps, omissions in the backups. A granular database backup history is below.
+--sql2012 and above
 select 
-	  a.database_name
-	, a.backuptype 
+	database_Name
+	, backuptype 
 	, d.recovery_model_desc
-	, LatestBackupStartDate = max(a.BackupStartDate)
-	, LatestBackupFinishDate = max(a.BackupFinishDate)
-	, LatestBackupLocation = max(a.physical_device_name)
-	, backup_size_mb			 = max(backup_size_mb)
-	, compressed_backup_size_mb	 = max(compressed_backup_size_mb)
+	, BackupDate = MAX(BackupDate)
 	, d.state_desc
 	, d.is_read_only
-	, d.create_date
+	, dm.Replica_Role		--SQL 2012+
  from sys.databases d
- inner join (	select * from (
-						select  
-						  database_name
-						, backuptype = case type	WHEN 'D' then 'Full Database Backup'
-												WHEN 'I' then 'Differential database backup'
-												WHEN 'L' then 'Transaction Log Backup'
-												WHEN 'F' then 'File or filegroup'
-												WHEN 'G' then 'Differential file'
-												WHEN 'P' then 'Partial'
-												WHEN 'Q' then 'Differential partial' END
-						, BackupFinishDate	=	bs.backup_finish_date
-						, BackupStartDate	=	bs.backup_start_date
-						, physical_device_name 
-						, backup_size_mb			=	bs.backup_size / 1024./1024.
-						, compressed_backup_size_mb =	bs.compressed_backup_size /1024./1024.
-	
-						, latest = Row_number() OVER (PARTITION BY database_name, type order by backup_finish_date desc)
-						from msdb.dbo.backupset bs					
-						left outer join msdb.dbo.[backupmediafamily] bf
-						on bs.[media_set_id] = bf.[media_set_id]	
-						WHERE backup_finish_date is not null 
-						) x
-						where latest = 1
-					 UNION 
-					 select 
-						db_name(d.database_id)
-						, backuptype = 'Full Database Backup'
-						, null, null, null, null, null, null
-						FROM master.sys.databases d
-						group by db_name(d.database_id)
-					 UNION
-					 select 
-						db_name(d.database_id)
-						, backuptype = 'Transaction Log Backup'
-						, null, null, null, null, null, null
-					  FROM master.sys.databases d
-					  where d.recovery_model_desc in ('FULL', 'BULK_LOGGED')
-					  group by db_name(d.database_id)
+ inner join 
+ (
+select distinct 
+	database_name
+	, database_id = db_id(database_name)
+	, backuptype = case type	WHEN 'D' then 'Database'
+							WHEN 'I' then 'Differential database'
+							WHEN 'L' then 'Transaction Log'
+							WHEN 'F' then 'File or filegroup'
+							WHEN 'G' then 'Differential file'
+							WHEN 'P' then 'Partial'
+							WHEN 'Q' then 'Differential partial' END
+	, BackupDate	=	MAX(backup_finish_date)  	
+	from msdb.dbo.backupset bs							
+ group by Database_name, type
+ UNION 
+ select distinct
+	db_name(d.database_id)
+	, d.database_id
+	, backuptype = 'Database'
+	, null
+	FROM master.sys.databases d
+ UNION
+ select distinct
+	db_name(d.database_id)
+	, d.database_id
+	, backuptype = 'Transaction Log'
+	, null
+  FROM master.sys.databases d
+  where d.recovery_model_desc in ('FULL', 'BULK_LOGGED')
  ) a
- on db_name(d.database_id) = a.database_name
- WHERE a.database_name not in ('tempdb')
-group by 
-	  a.database_name
-	, a.backuptype 
-	, d.recovery_model_desc
-	, d.state_desc
-	, d.is_read_only
-	, d.create_date
-order by a.backuptype, d.recovery_model_desc, max(a.BackupFinishDate) asc, a.database_name asc
- go
+ on d.database_id = a.database_id
  
+ --SQL 2012+
+ LEFT OUTER JOIN ( SELECT  database_id 
+						 ,	Replica_Role		= CASE WHEN database_state_desc IS NOT NULL and last_received_time is null THEN 'PRIMARY '
+															WHEN database_state_desc IS NOT NULL and last_received_time is not null THEN 'SECONDARY' 
+															ELSE null END
+															from sys.dm_hadr_database_replica_states) dm
+						 on dm.database_id = a.database_id
+WHERE database_name not in ('model','tempdb')
+and not (backuptype = 'transaction log' and recovery_model_desc = 'SIMPLE')
+group by database_name, backuptype, d.recovery_model_desc, d.state_desc, d.is_read_only, dm.replica_role
+order by backuptype, recovery_model_desc, database_name asc
+go
 
  /*
 --for SQL 2000 and above
@@ -146,3 +138,34 @@ SELECT
 	order by Latest_Restore desc
 
 */
+
+
+--Look for backups to NUL, a sign that someone doesn't know what they're doing to the tlog. (Probably VEEAM. Bad VEEAM.)
+--This is bad. Backup to NUL is just truncating the log without backing up the log, breaking the tlog chain. Any subsequent tlog backups are broken until a FULL backup restarts a valid chain.
+--Do not allow VEEAM or other VSS-based backup solutions to do backups to NUL. 
+--In VEEAM, this is somewhere near the "application aware backups" or similar settings menu in various settings. Disable this. 
+SELECT 
+	  bs.database_name
+	, backuptype = CASE 
+							WHEN bs.type = 'D' and bs.is_copy_only = 0 then 'Full Database'
+							WHEN bs.type = 'D' and bs.is_copy_only = 1 then 'Full Copy-Only Database'
+							WHEN bs.type = 'I' then 'Differential database backup'
+							WHEN bs.type = 'L' then 'Transaction Log'
+							WHEN bs.type = 'F' then 'File or filegroup'
+							WHEN bs.type = 'G' then 'Differential file'
+							WHEN bs.type = 'P' then 'Partial'
+							WHEN bs.type = 'Q' then 'Differential partial' END + ' Backup'
+	, bs.recovery_model
+	, BackupStartDate = bs.Backup_Start_Date
+	, BackupFinishDate = bs.Backup_Finish_Date
+	, LatestBackupLocation = bf.physical_device_name
+	, backup_size_mb			=	bs.backup_size / 1024./1024.
+	, compressed_backup_size_mb =	bs.compressed_backup_size /1024./1024.
+	, database_backup_lsn -- For tlog and differential backups, this is the checkpoint_lsn of the FULL backup it is based on. 
+	, checkpoint_lsn
+	, begins_log_chain
+	FROM msdb.dbo.backupset bs	
+	LEFT OUTER JOIN msdb.dbo.[backupmediafamily] bf
+	on bs.[media_set_id] = bf.[media_set_id]
+	where bf.physical_device_name = 'NUL'
+	ORDER BY  bs.database_name asc, bs.Backup_Start_Date desc;
--- multiserver backup history.sql
+++ multiserver backup history.sql
@@ -1,4 +1,8 @@
---sql2005 and above
+--This version of backup history.sql only looks for problematic backup gaps, etc.
+--Includes filters for a lack of recent backups
+--Include AG group status. Be aware of the AG's replica backup preference, make sure backups are happening somewhere. They may not happen on the PRIMARY.
+
+--sql2012 and above
 select 
 	database_Name
 	, backuptype 
@@ -6,11 +10,14 @@ select
 	, BackupDate = MAX(BackupDate)
 	, d.state_desc
 	, d.is_read_only
+	, dm.Replica_Role		--SQL 2012+
+
  from sys.databases d
  inner join 
  (
 select distinct 
 	database_name
+	, database_id = db_id(database_name)
 	, backuptype = case type	WHEN 'D' then 'Database'
 							WHEN 'I' then 'Differential database'
 							WHEN 'L' then 'Transaction Log'
@@ -24,23 +31,73 @@ select distinct
  UNION 
  select distinct
 	db_name(d.database_id)
+	, d.database_id
 	, backuptype = 'Database'
 	, null
 	FROM master.sys.databases d
  UNION
  select distinct
 	db_name(d.database_id)
+	, d.database_id
 	, backuptype = 'Transaction Log'
 	, null
   FROM master.sys.databases d
   where d.recovery_model_desc in ('FULL', 'BULK_LOGGED')
-  
  ) a
- on db_name(d.database_id) = a.database_name
- where database_name not in ('model','tempdb')
- group by database_name, backuptype, d.recovery_model_desc, d.state_desc
-	, d.is_read_only
- having ( (max(backupdate) <= dateadd(day,-1,getdate()) and (max(backupdate) >= dateadd(month, -1, getdate())) ) or max(backupdate) is null)
+ on d.database_id = a.database_id
+
+ --below is SQL 2012+
+ LEFT OUTER JOIN ( SELECT  database_id 
+						 ,	Replica_Role		= CASE WHEN database_state_desc IS NOT NULL and last_received_time is null THEN 'PRIMARY '
+															WHEN database_state_desc IS NOT NULL and last_received_time is not null THEN 'SECONDARY' 
+															ELSE null END
+															from sys.dm_hadr_database_replica_states) dm
+						 on dm.database_id = a.database_id
+
+WHERE database_name not in ('model','tempdb')
+and state_desc <> 'offline'
+and not (backuptype = 'transaction log' and recovery_model_desc = 'SIMPLE')
+ group by database_name, backuptype, d.recovery_model_desc, d.state_desc, d.is_read_only, dm.replica_role
+
+ having ( 
+			(   max(backupdate) <= dateadd(week,-1,getdate()) 
+				and backuptype = 'database'
+			) 
+			or 
+			(   max(backupdate) <= dateadd(hour,-3,getdate()) 
+				and backuptype = 'transaction log'
+			) 
+			or (	max(backupdate) is null		)
+		)
 order by backuptype, recovery_model_desc, database_name asc
- 
- go
+go
+
+
+
+--Look for tlog backups to NUL, a sign that someone doesn't know what they're doing to the tlog. (Probably VEEAM)
+
+SELECT 
+	  bs.database_name
+	, backuptype = CASE 
+							WHEN bs.type = 'D' and bs.is_copy_only = 0 then 'Full Database'
+							WHEN bs.type = 'D' and bs.is_copy_only = 1 then 'Full Copy-Only Database'
+							WHEN bs.type = 'I' then 'Differential database backup'
+							WHEN bs.type = 'L' then 'Transaction Log'
+							WHEN bs.type = 'F' then 'File or filegroup'
+							WHEN bs.type = 'G' then 'Differential file'
+							WHEN bs.type = 'P' then 'Partial'
+							WHEN bs.type = 'Q' then 'Differential partial' END + ' Backup'
+	, bs.recovery_model
+	, BackupStartDate = bs.Backup_Start_Date
+	, BackupFinishDate = bs.Backup_Finish_Date
+	, LatestBackupLocation = bf.physical_device_name
+	, backup_size_mb			=	bs.backup_size / 1024./1024.
+	, compressed_backup_size_mb =	bs.compressed_backup_size /1024./1024.
+	, database_backup_lsn -- For tlog and differential backups, this is the checkpoint_lsn of the FULL backup it is based on. 
+	, checkpoint_lsn
+	, begins_log_chain
+	FROM msdb.dbo.backupset bs	
+	LEFT OUTER JOIN msdb.dbo.[backupmediafamily] bf
+	on bs.[media_set_id] = bf.[media_set_id]
+	where bf.physical_device_name = 'NUL'
+	ORDER BY  bs.database_name asc, bs.Backup_Start_Date desc;
