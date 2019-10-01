20c357e6579d32c6e0c88139ab62d51b0c365d41
william assaf <6641470+williamadba@users.noreply.github.com>
2019-10-01 16:30:40 -0500

    Create lab - float at pk problems.sql

---
+++ lab - float at pk problems.sql
@@ -0,0 +1,50 @@
+drop table if exists dbo.floatinghorror;
+drop table if exists dbo.migrated_table;
+go
+create table dbo.floatinghorror
+(invoiceid float not null constraint PK_floatinghorror primary key
+, whatever varchar(10) not null);
+go
+--1 thru 8
+insert into dbo.floatinghorror (invoiceid, whatever)
+values (2019000000000.001, char(79));
+insert into dbo.floatinghorror (invoiceid, whatever)
+values (2019000000000.002, char(77));
+insert into dbo.floatinghorror (invoiceid, whatever)
+values (2019000000000.003, char(71));
+insert into dbo.floatinghorror (invoiceid, whatever)
+values (2019000000000.004, char(33));
+GO
+insert into dbo.floatinghorror (invoiceid, whatever)
+values (2019000000100.005, char(87));
+insert into dbo.floatinghorror (invoiceid, whatever)
+values (2019000000100.006, char(84));
+insert into dbo.floatinghorror (invoiceid, whatever)
+values (2019000000100.007, char(70));
+insert into dbo.floatinghorror (invoiceid, whatever)
+values (2019000000100.008, char(33));
+
+GO
+
+select invoiceid from dbo.floatinghorror --weird
+select distinct invoiceid from dbo.floatinghorror --more weird, aggregates correctly?!
+select invoiceid, count(1) from dbo.floatinghorror group by invoiceid --still weird, aggregates correctly?!
+select cast(invoiceid as decimal(19,3)), count(1) from dbo.floatinghorror group by invoiceid --to display the right data, gotta convert
+GO
+
+--Say we have to migrate the data to another system, like a data warehouse or a competitor's software, 
+--to a database that uses grown-up data types
+--and they have higher precision requirements!
+create table dbo.migrated_table
+(invoiceid decimal(19,4) not null constraint PK_migrated_table primary key
+, whatever varchar(10) not null
+)
+--the data inserts fine
+insert into dbo.migrated_table (invoiceid, whatever)
+select invoiceid, whatever from dbo.floatinghorror 
+GO
+select * from dbo.migrated_table
+--but oh no!
+
+
+
