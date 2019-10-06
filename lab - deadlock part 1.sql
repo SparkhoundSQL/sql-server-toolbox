596a59d6177c6eee5e86be8715bee9153b54279b
william assaf <6641470+williamadba@users.noreply.github.com>
2019-10-06 12:54:31 -0500

    add comments

--- lab - deadlock part 1.sql
+++ lab - deadlock part 1.sql
@@ -1,6 +1,8 @@
+--To generate a deadlock: 
+--Run the first half of this script first, then run toolbox\lab - deadlock part 2.sql, then the second half of this script, then the second part of this script.
+--Then use toolbox\deadlocks in xevents.sql to view the deadlock.
 use w
 go
-
 DROP TABLE dbo.dead
 DROP TABLE dbo.lock 
 go
@@ -11,7 +13,9 @@ INSERT INTO dbo.lock SELECT 1
 
 BEGIN TRAN t1
 UPDATE dbo.dead WITH (TABLOCK) SET col1 = 2
---
+
+-- Part two, run the below after script 2.
+
 UPDATE dbo.lock WITH (TABLOCK) SET col1 = 4
 COMMIT TRAN t1
 GO
