b2bb05076c39ea667af7e6d898adc5612fcaaf03
william assaf <6641470+williamadba@users.noreply.github.com>
2019-10-08 16:47:16 -0500

    better comment for 2014+ feature

--- last known DBCC CHECKDB.sql
+++ last known DBCC CHECKDB.sql
@@ -1,5 +1,7 @@
 EXEC sp_MSforeachdb '
 --Table variable to capture the DBCC DBINFO output, look for the field we want in each database output
-DECLARE @DBCC_DBINFO TABLE (ParentObject VARCHAR(255) NOT NULL, [Object] VARCHAR(255)  NOT NULL, [Field] VARCHAR(255) NOT NULL INDEX idx_dbinfo_field CLUSTERED, [Value] VARCHAR(255));
+DECLARE @DBCC_DBINFO TABLE (ParentObject VARCHAR(255) NOT NULL, [Object] VARCHAR(255)  NOT NULL, [Field] VARCHAR(255) NOT NULL 
+INDEX idx_dbinfo_field CLUSTERED --just this line is SQL 2014+ only
+, [Value] VARCHAR(255));
 INSERT INTO @DBCC_DBINFO EXECUTE ("DBCC DBINFO ([?]) WITH TABLERESULTS");
-SELECT ''?'', [Value] FROM @DBCC_DBINFO WHERE Field = ''dbi_dbccLastKnownGood'';';
+SELECT DISTINCT ''?'', [Value] FROM @DBCC_DBINFO WHERE Field = ''dbi_dbccLastKnownGood'';';
