16140d85ca29e9fe79d509c11b66dac2fa4d349f
william assaf <6641470+williamadba@users.noreply.github.com>
2020-02-19 18:08:32 -0600

    update comments

--- change tracking autocleanup diagnostic.sql
+++ change tracking autocleanup diagnostic.sql
@@ -9,8 +9,8 @@ SELECT
 FROM sys.dm_tran_commit_table;
 
 --The minimum_commit_time should progress forward periodically.  
---As default configured with 7 days retention, the minimum_commit_time should be slightly more than 7 days ago if CT is keeping up.
+--As default configured with 2 days retention, the minimum_commit_time should be slightly more than 2 days ago if CT is keeping up.
 
 --CT may get behind in the autocleanup. This is not a severe problem but will surface with:
 --Error: 22123, Severity: 16, State: 1.
---Change Tracking autocleanup is blocked on side table of "tablename". If the failure persists, check whether the table "tablename is blocked by any process.
+--Change Tracking autocleanup is blocked on side table of "tablename". If the failure persists, check whether the table "tablename" is blocked by any process, like in toolbox\uncommitted transaction.sql.
