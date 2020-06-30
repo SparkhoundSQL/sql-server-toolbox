33d0b8f04fbd59dd9d311a4a528fd0765d286a8c
william assaf <6641470+williamadba@users.noreply.github.com>
2020-06-30 11:04:02 -0500

    Update uncommitted transactions.sql
    add all db option

--- uncommitted transactions.sql
+++ uncommitted transactions.sql
@@ -1,4 +1,11 @@
 --Returns information on uncommitted transactions
 
+--current database
 select * from sys.dm_tran_active_transactions tat 
 inner join sys.dm_tran_session_transactions tst  on tat.transaction_id = tst.transaction_id
+
+
+--all databases
+exec sp_msforeachdb N'use [?]; select database_name = ''?'', * FROM sys.dm_tran_active_transactions tat 
+INNER JOIN sys.dm_tran_session_transactions tst  on tat.transaction_id = tst.transaction_id
+INNER JOIN sys.dm_exec_connections ec ON tst.session_id = ec.session_id'
