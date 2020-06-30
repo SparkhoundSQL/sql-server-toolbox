c24d457c939bad663ae7c04932ce2cfc849966cb
william assaf <6641470+williamadba@users.noreply.github.com>
2020-06-30 14:38:05 -0500

    Update lock Pages in Memory LPIM.sql
    comments

--- lock Pages in Memory LPIM.sql
+++ lock Pages in Memory LPIM.sql
@@ -6,3 +6,9 @@ select sql_memory_model_Desc
 --LOCK_PAGES = Lock Pages in Memory privilege is granted
 --LARGE_PAGES = Lock Pages in Memory privilege is granted in Enterprise mode with Trace Flag 834 ON
 from sys.dm_os_sys_info;
+
+
+/* 
+If LPIM is enabled, MAX SERVER MEMORY MUST BE CONFIGURED conservatively. It WILL cause problems with Windows if memory is exhausted.
+FWIW LPIM not used in Azure, because they do not have issues with working set trim problems. 
+*/
