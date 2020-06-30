9aeab538d7a6097e0a27729bf72e359cf6d2d38d
william assaf <6641470+williamadba@users.noreply.github.com>
2020-06-30 11:04:42 -0500

    Update lock Pages in Memory LPIM.sql

--- lock Pages in Memory LPIM.sql
+++ lock Pages in Memory LPIM.sql
@@ -1,8 +1,8 @@
-
 --SQL 2016 SP1 or above only!!!
-
+use TempDB;
+GO
 select sql_memory_model_Desc 
 --Conventional = Lock Pages in Memory privilege is not granted
 --LOCK_PAGES = Lock Pages in Memory privilege is granted
 --LARGE_PAGES = Lock Pages in Memory privilege is granted in Enterprise mode with Trace Flag 834 ON
-from sys.dm_os_sys_info 
+from sys.dm_os_sys_info;
