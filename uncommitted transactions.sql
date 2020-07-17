c73bb36dc89310c4fc23c2bbc3aee4b77687c050
william assaf <6641470+williamadba@users.noreply.github.com>
2020-07-17 12:08:35 -0500

    Update uncommitted transactions.sql

--- uncommitted transactions.sql
+++ uncommitted transactions.sql
@@ -1,11 +1,16 @@
 --Returns information on uncommitted transactions
 
 --current database
-select * from sys.dm_tran_active_transactions tat 
-inner join sys.dm_tran_session_transactions tst  on tat.transaction_id = tst.transaction_id
+select Observed = sysdatetimeoffset(), *  FROM sys.dm_tran_active_transactions tat 
+INNER JOIN sys.dm_tran_session_transactions tst  on tat.transaction_id = tst.transaction_id
+INNER JOIN sys.dm_exec_connections ec ON tst.session_id = ec.session_id
+INNER JOIN Sys.dm_exec_requests r on r.transaction_id = tst.transaction_id
+WHERE r.database_id = db_id()
 
 
 --all databases
-exec sp_msforeachdb N'use [?]; select database_name = ''?'', * FROM sys.dm_tran_active_transactions tat 
+exec sp_msforeachdb N'use [?]; select database_name = ''?'', Observed = sysdatetimeoffset(), * FROM sys.dm_tran_active_transactions tat 
 INNER JOIN sys.dm_tran_session_transactions tst  on tat.transaction_id = tst.transaction_id
-INNER JOIN sys.dm_exec_connections ec ON tst.session_id = ec.session_id'
+INNER JOIN sys.dm_exec_connections ec ON tst.session_id = ec.session_id
+INNER JOIN Sys.dm_exec_requests r on r.transaction_id = tst.transaction_id
+WHERE r.database_id = db_id()'
