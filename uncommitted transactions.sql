044719d4c66bdcb86905252af1d2bb8329a47e54
william assaf <6641470+williamadba@users.noreply.github.com>
2020-09-02 14:32:36 -0500

    Update uncommitted transactions.sql

--- uncommitted transactions.sql
+++ uncommitted transactions.sql
@@ -1,6 +1,6 @@
 --Returns information on uncommitted transactions
 
---current database
+--current database (see below for all databases)
 select Observed = sysdatetimeoffset(), tat.transaction_id, tat.transaction_begin_time, tst.session_id 
 ,	[database_name]	= db_name(s.database_id)
 ,	transaction_duration_s = datediff(s, tat.transaction_begin_time, sysdatetime())
@@ -33,43 +33,76 @@ select Observed = sysdatetimeoffset(), tat.transaction_id, tat.transaction_begin
 ,	session_open_transaction_count = tst.open_transaction_count --uncommitted and unrolled back transactions open. 
 ,	s.login_time, s.host_name, s.program_name, s.client_interface_name, s.login_name, s.is_user_process, s.cpu_time, s.logical_reads, s.reads, s.writes
 ,	request_status = r.status
---, *
+
 FROM sys.dm_tran_active_transactions tat 
 INNER JOIN sys.dm_tran_session_transactions tst  on tat.transaction_id = tst.transaction_id
 INNER JOIN Sys.dm_exec_sessions s on s.session_id = tst.session_id 
 LEFT OUTER JOIN sys.dm_exec_requests r on r.session_id = s.session_id
 CROSS APPLY sys.dm_exec_input_buffer(s.session_id, null) AS ib 
-WHERE s.database_id = db_id()
+WHERE s.database_id = db_id() and s.session_id <> @@SPID
 
 
+/*
+--Optional, insert into table.
+CREATE TABLE [dbo].[uncommitted_transactions](
+	ID INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_uncommitted_transactions PRIMARY KEY,
+	[Observed] [datetimeoffset](7) NOT NULL,
+	[transaction_id] [bigint] NOT NULL,
+	[transaction_begin_time] [datetime] NOT NULL,
+	[session_id] [int] NOT NULL,
+	[database_name] [nvarchar](128) NULL,
+	[transaction_duration_s] [int] NULL,
+	[transaction_type] [varchar](23) NULL,
+	[input_buffer] [nvarchar](max) NULL,
+	[transaction_uow] [uniqueidentifier] NULL,
+	[transaction_state] [varchar](198) NULL,
+	[transaction_name] [nvarchar](32) NOT NULL,
+	[azure_dtc_state] [varchar](9) NULL,
+	[is_user_transaction] [bit] NOT NULL,
+	[is_local] [bit] NOT NULL,
+	[session_open_transaction_count] [int] NOT NULL,
+	[login_time] [datetime] NOT NULL,
+	[host_name] [nvarchar](128) NULL,
+	[program_name] [nvarchar](128) NULL,
+	[client_interface_name] [nvarchar](32) NULL,
+	[login_name] [nvarchar](128) NOT NULL,
+	[is_user_process] [bit] NOT NULL,
+	[cpu_time] [int] NOT NULL,
+	[logical_reads] [bigint] NOT NULL,
+	[reads] [bigint] NOT NULL,
+	[writes] [bigint] NOT NULL,
+	[request_status] [nvarchar](30) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+*/
 --all databases
-exec sp_msforeachdb N' 
+--Note optional INSERT 
+exec sp_msforeachdb N'insert into dbo.uncommitted_transactions
 select Observed = sysdatetimeoffset(), tat.transaction_id, tat.transaction_begin_time, tst.session_id 
 ,	database_name	= db_name(s.database_id) 
 ,	trn_duration_s = datediff(s, tat.transaction_begin_time, sysdatetime())
-,	trn_type = CASE tat.transaction_type	WHEN 1 THEN ''Read/write tran''
-												WHEN 2 THEN ''Read-only''
-												WHEN 3 THEN ''System''
-												WHEN 4 THEN ''Distributed trn'' END
+,	trn_type = CASE tat.transaction_type WHEN 1 THEN ''Read/write tran''
+	WHEN 2 THEN ''Read-only''
+	WHEN 3 THEN ''System''
+	WHEN 4 THEN ''Distributed trn'' END
 ,	input_buffer	= ib.event_info
 ,	transaction_uow	 
-,	trn_state= CASE tat.transaction_state	
-						WHEN 0 THEN ''trn has not been completely initialized yet.''
-						WHEN 1 THEN ''trn has been initialized but has not started.''
-						WHEN 2 THEN ''trn is active - has not been committed or rolled back.''
-						WHEN 3 THEN ''trn has ended. This is used for read-only trns.''
-						WHEN 4 THEN ''commit process has been initiated on the distributed trn.''
-						WHEN 5 THEN ''trn is in a prepared state and waiting resolution.''
-						WHEN 6 THEN ''trn has been committed.''
-						WHEN 7 THEN ''trn is being rolled back.''
-						WHEN 8 THEN ''trn has been rolled back.'' END 
+,	trn_state= CASE tat.transaction_state WHEN 0 THEN ''trn has not been completely initialized yet.''
+WHEN 1 THEN ''trn has been initialized but has not started.''
+WHEN 2 THEN ''trn is active - has not been committed or rolled back.''
+WHEN 3 THEN ''trn has ended. This is used for read-only trns.''
+WHEN 4 THEN ''commit process has been initiated on the distributed trn.''
+WHEN 5 THEN ''trn is in a prepared state and waiting resolution.''
+WHEN 6 THEN ''trn has been committed.''
+WHEN 7 THEN ''trn is being rolled back.''
+WHEN 8 THEN ''trn has been rolled back.'' END 
 ,	trn_name = tat.name
-,	azure_dtc_state	 =	CASE dtc_state 
-					WHEN 1 THEN ''ACTIVE''
-					WHEN 2 THEN ''PREPARED''
-					WHEN 3 THEN ''COMMITTED''
-					WHEN 4 THEN ''ABORTED''
-					WHEN 5 THEN ''RECOVERED'' END
+,	azure_dtc_state	 =	CASE dtc_state WHEN 1 THEN ''ACTIVE''
+WHEN 2 THEN ''PREPARED''
+WHEN 3 THEN ''COMMITTED''
+WHEN 4 THEN ''ABORTED''
+WHEN 5 THEN ''RECOVERED'' END
 ,	tst.is_user_transaction, tst.is_local
 ,	session_open_trn_count = tst.open_transaction_count  
 ,	s.login_time, s.host_name, s.program_name, s.client_interface_name, s.login_name, s.is_user_process, s.cpu_time, s.logical_reads, s.reads, s.writes
@@ -79,5 +112,7 @@ INNER JOIN sys.dm_tran_session_transactions tst  on tat.transaction_id = tst.tra
 INNER JOIN Sys.dm_exec_sessions s on s.session_id = tst.session_id 
 LEFT OUTER JOIN sys.dm_exec_requests r on r.session_id = s.session_id
 CROSS APPLY sys.dm_exec_input_buffer(s.session_id, null) AS ib 
-WHERE db_name(s.database_id)=''?''
+WHERE db_name(s.database_id)=''?'' and s.session_id <> @@SPID
 '
+go
+select * from dbo.uncommitted_transactions
