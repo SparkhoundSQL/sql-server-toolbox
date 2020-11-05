9279f0095b10a91644fed164a88c98905a1162f5
william assaf <6641470+williamadba@users.noreply.github.com>
2020-11-05 13:26:40 -0600

    Update get disk block allocation size.ps1

--- get disk block allocation size.ps1
+++ get disk block allocation size.ps1
@@ -1,7 +1,8 @@
 ﻿##Get Block size per 
+##Look for Bytes Per Cluster. By default 4096, should be 65536 for SQL data, logs, and tempdb volumes.
 Fsutil fsinfo ntfsinfo d: 
 Fsutil fsinfo ntfsinfo e: 
-##...
+##Run once per volume, etc.
 
 ##Get Starting Offset
 ##wmic partition get BlockSize, StartingOffset, Name, Index
